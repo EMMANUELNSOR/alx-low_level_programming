9df597707c3cab1c3628f091b191085d94991616
EMMANUELNSOR <emmanuelnsor@gmail.com>
2022-12-08 05:55:54 -0800

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <conio.h>
 /**
  * main -Entry point
  * Return Always 0 (Success)
@@ -7,10 +6,9 @@
 int main(void)
 {
 	printf("size of char: %lu byte(s)\n", sizeof(char));
-	printf("size of int:%lu byte(s)\n", sizeof(int));
-	printf("size of a long int:%lu byte(s)\n", sizeof(long int));
-	printf("size of a long long  int:%lu byte(s)\n", sizeof(long long int));
-	printf("size of float:%lu byte(s)\n", sizeof(float));
-	getch();
+	printf("size of int: %lu byte(s)\n", sizeof(int));
+	printf("size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("size of a long long  int: %lu byte(s)\n", sizeof(long long int));
+	printf("size of float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
