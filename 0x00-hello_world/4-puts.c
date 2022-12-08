4d4727da098de2a11018e6621bf804feab9eb9d9
EMMANUELNSOR <emmanuelnsor@gmail.com>
2022-12-08 11:07:17 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry point 
- * Return: Always 0 (Success)
+ * main - Entry
+ * Return: Always 0
  */
 int main(void)
 {
-	puts("\" Programming is like buliding a multilingual puzzle");
+	puts("\"Programming ia like building a multilingual puzzle");
 	return (0);
 }
