c788233821786b04223d63174c7cda3f03086112
EMMANUELNSOR <emmanuelnsor@gmail.com>
2022-12-14 05:27:07 -0800

    1 main.c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main. cause an infinite loop
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("infinite loop incoming :(\n");
+
+	i=0;
+
+	/*
+	 * while (i<10)
+	 * {
+	 * puchar(i)
+	 * }
+	 */
+	print("infinite loop avoided! \\o/\n");
+	return(0);
+}
