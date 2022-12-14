ddd12e2dcb773bd76328a8516723e1b665610fde
EMMANUELNSOR <emmanuelnsor@gmail.com>
2022-12-13 19:08:09 -0800

    1 alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,15 +2,15 @@
 
 
 /**
- * print_alphabet - print all alphabet in lowercase
+ * print_alphabet - prints the alpha in lowercase
  */
-
 void print_alphabet(void)
 {
-	char letter;
+	char la;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
 
 	_putchar('\n');
 }
