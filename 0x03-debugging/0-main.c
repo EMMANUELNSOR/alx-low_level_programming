22ebded1ba38a6843cf5ab71424df29efd2a443a
EMMANUELNSOR <emmanuelnsor@gmail.com>
2022-12-14 05:03:09 -0800

    0 main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main -tests function that print if interger is positive or nagative
+ * Return: Zero
+ */
+int main(void)
+{
+	int i;
+	i=0;
+	positive_nagative_(i);
+	return(0);
+}
+
