1f58931a7a593c55bedd6c18601f3a5bf437ab4d
EMMANUELNSOR <emmanuelnsor@gmail.com>
2022-12-16 06:04:39 -0800

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,7 +7,7 @@
  */
 void print_number(int n)
 {
-	unsigned int m, d, count
+	unsigned int m, d, count;
 
 	if (n < 0)
 	{
@@ -21,15 +21,13 @@ void print_number(int n)
 
 	d = m;
 	count = 1;
-
 	while (d > 9)
 	{
 		d /= 10;
 		count *= 10;
 	}
-
 	for (; count >= 1; count /= 10)
 	{
-		_putchar(((m / count) % 10) + 48);
+		_putchar(((m / count) % 10) % 10) + 48);
 	}
 }
